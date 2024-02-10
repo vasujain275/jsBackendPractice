593047ff0a77ac9f78b13cdc26943743a87bd22b
vasujain275 <vasujain275@gmail.com>
2024-02-10 23:24:04 +0530

    Added utils

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import connectDB from "./db/connectDB.js";
+import dotenv from "dotenv";
+import { app } from "./app.js";
+
+dotenv.config({
+    path: "./.env",
+});
+
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERR: ", error);
+            throw error;
+        });
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MongoDB Connection Failed - ", err);
+    });
