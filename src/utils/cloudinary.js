4b23701b6294cc419922c158863cff0949d7e169
vasujain275 <vasujain275@gmail.com>
2024-02-11 15:17:12 +0530

    feat: Multer and Cloudinary Support

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+
+        // Upload the File
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+        // File Uploaded Successfully
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+export { uploadOnCloudinary }
