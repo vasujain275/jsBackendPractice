c228131ee79bdccb588c9b4f89e3b91d02970bb7
vasujain275 <vasujain275@gmail.com>
2024-02-16 21:39:42 +0530

    refactor: user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -81,7 +81,7 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-  const { username, email } = req.body;
+  const { username, email, password } = req.body;
 
   if (!username || !email) {
     throw new ApiError(400, "Either Username or Email is required!!");
