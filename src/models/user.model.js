435d38bd623572c311c42f7578bcb2ad1ec276c2
vasujain275 <vasujain275@gmail.com>
2024-03-18 23:25:50 +0530

    fixed password hashing bug

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -48,18 +48,18 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-userSchema.pre("save", async function (next) {
-  if (this.isModified("password")) return next();
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) return next();
 
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-userSchema.methods.isPasswordCorrect = async function (password) {
+userSchema.methods.isPasswordCorrect = async function(password) {
   return await bcrypt.compare(password, this.password);
 };
 
-userSchema.methods.generateAccessToken = function () {
+userSchema.methods.generateAccessToken = function() {
   return jwt.sign(
     {
       _id: this._id,
@@ -73,7 +73,7 @@ userSchema.methods.generateAccessToken = function () {
     }
   );
 };
-userSchema.methods.generateRefreshToken = function () {
+userSchema.methods.generateRefreshToken = function() {
   return jwt.sign(
     {
       _id: this._id,
@@ -84,6 +84,5 @@ userSchema.methods.generateRefreshToken = function () {
     }
   );
 };
-userSchema.methods.generateRefreshToken = function () {};
 
 export const User = model("User", userSchema);
